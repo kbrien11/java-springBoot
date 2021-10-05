6d532b29c952793767b5a80ff68a7de176c6a588
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-10-05 09:56:58 -0400

    Update location.java

--- weather/src/main/java/com/example/weather/location.java
+++ weather/src/main/java/com/example/weather/location.java
@@ -2,30 +2,58 @@ package com.example.weather;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Location")
 public class location {
 	
 	
-	
+	@Id
 	private long id;
+	@Column(name = "city")
 	private String city;
-	private String state;
+	@Column(name = "state")
+	private  String state;
 	
 	public location() {
 		
 	}
 	
 	public location(String city , String state) {
+		
+		this.city = city;
+		this.state = state;
+		
+	}
+public location(long id,String city , String state) {
+		this.id = id;
 		this.city = city;
 		this.state = state;
 		
 	}
 	
-	public String getCity() {
+	public String getCity(String city2) {
 		return this.city;
 		
 	}
 	
-	public String getState() {
+	public long getId() {
+		return this.id;
+	}
+	public long setId() {
+		return this.id;
+	}
+	
+	public  String getState() {
+		return this.state;
+	}
+	public  String getState(String city) {
 		return this.state;
 	}
 	
@@ -34,6 +62,14 @@ public class location {
 	}
 	
 	public String setState() {
+		return this.state = state;
+	}
+	
+	public String setCity(String city) {
+		return this.city = city;
+	}
+	
+	public String setState(String state) {
 		return this.state = state;
 	}
 	@Override
