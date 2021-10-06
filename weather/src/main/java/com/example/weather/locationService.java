edb8fc007989cad628f7cc866a5bf23dc461b2b6
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-10-06 15:16:58 -0400

    Add files via upload

---
+++ weather/src/main/java/com/example/weather/locationService.java
@@ -0,0 +1,28 @@
+package com.example.weather;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class locationService {
+	
+	private final locationRespository locationRepo;
+	
+	@Autowired
+	public locationService(locationRespository locationRepo) {
+		this.locationRepo = locationRepo;
+	}
+	
+	public  location addNewLocation(location location) throws Exception {
+	 List<location> findCity = 	locationRepo.findByCity(location.getCity());
+	 
+	 if(findCity.size()>0) {
+		 throw new Exception("city already exsits in the DB");
+	 }
+		return locationRepo.save(location);
+	}
+
+}
