7dc40749e94b44f8af8ec87001332e4b775a564d
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-10-11 15:08:40 -0400

    Update location.java

--- weather/src/main/java/com/example/weather/location.java
+++ weather/src/main/java/com/example/weather/location.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name = "cities")
+@Table(name = "weather")
 public class location {
 	
 	
@@ -22,6 +22,8 @@ public class location {
 	private String city;
 	@Column(name = "state")
 	private  String state;
+	@Column(name = "favorite")
+	private int favorite;
 	
 	public location() {
 		
@@ -31,13 +33,13 @@ public class location {
 		
 		this.city = city;
 		this.state = state;
-		
+
 	}
-public location(long id,String city , String state) {
+public location(long id,String city , String state, int favorite) {
 		this.id = id;
 		this.city = city;
 		this.state = state;
-		
+		this.favorite = favorite;
 	}
 	
 	public String getCity(String city2) {
@@ -75,9 +77,18 @@ public location(long id,String city , String state) {
 		return this.city = city;
 	}
 	
+	public int getFavorite() {
+		return this.favorite = favorite;
+	}
+	
 	public String setState(String state) {
 		return this.state = state;
 	}
+	public int setFavorite() {
+		return this.favorite = favorite;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "location{" +
