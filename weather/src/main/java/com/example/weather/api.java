185dbf543874b3a4cc08106b3f04cd8aead50195
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-10-11 16:19:11 -0400

    Update api.java

--- weather/src/main/java/com/example/weather/api.java
+++ weather/src/main/java/com/example/weather/api.java
@@ -31,7 +31,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
   @SuppressWarnings("rawtypes")
- public  Object apiData(Long id, String city, String state) throws IOException{
+ public  Object apiData(Long id, String city, String state, int favorite) throws IOException{
 	ArrayList<Object> data = new ArrayList<Object>();
 	String ex = String.format("http://api.openweathermap.org/data/2.5/weather?q=%s&units=imperial&appid=27b3ec19c7d34c1bcca082098b7a60a7", city);
     URL quote_endpoint = new URL(ex);
@@ -55,7 +55,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 				JSONObject json = (JSONObject) parser.parse(jsonResponseData);
 				data.add(id);
 				data.add(json.get("name"));
-				 data.add(state);
+				data.add(state);
+		     		data.add(favorite);
 //				for(locati on x:location) {
 //					System.out.println(x);
 //				}
