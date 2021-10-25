ad4c3ac9990d106c8840cc199027fd5c9a250978
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-10-25 14:37:02 -0400

    Update addCityToDatabase.java

--- weather/src/main/java/com/example/weather/addCityToDatabase.java
+++ weather/src/main/java/com/example/weather/addCityToDatabase.java
@@ -1,5 +1,37 @@
 package com.example.weather;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class addCityToDatabase {
 
+	 public  List<location> cities(String city) throws FileNotFoundException, IOException {
+		
+		 List<location>locationList = new ArrayList<>();
+
+	
+			location loc = new location(
+					
+					city
+				
+					
+					);
+	
+			locationList.add(loc);
+		
+		 
+		 
+		 System.out.println(locationList);
+		 return locationList;	 
+	 }
+	 
+	
+	 
+	
+	
 }
